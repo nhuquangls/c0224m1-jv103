5440270fc4579ca9af8b87a2383c2529e5e51da5
test <nhuquangls@gmail.com>
2024-05-11 12:43:27 +0700

    update

--- 4.Class-objects/quadratic-equation/src/Main.java
+++ 4.Class-objects/quadratic-equation/src/Main.java
@@ -4,17 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập a: ");
-        double a = scanner.nextDouble();
+        QuadraticEquation.setA(scanner.nextDouble());
         System.out.println("Nhập b: ");
-        double b = scanner.nextDouble();
+        QuadraticEquation.setB(scanner.nextDouble());
         System.out.println("Nhập c: ");
-        double c = scanner.nextDouble();
+        QuadraticEquation.setC(scanner.nextDouble());
 
-        QuadraticEquation equation1 = new QuadraticEquation(a, b, c);
-        if (equation1.getDelta() == 0) {
-            System.out.println("Nghiệm của PT là: " + equation1.getRoot1());
-        } else if (equation1.getDelta() > 0) {
-            System.out.println("Nghiệm của PT là: " + equation1.getRoot1() + " và " + equation1.getRoot2());
+        if (QuadraticEquation.getDelta() == 0) {
+            System.out.println("Nghiệm của PT là: " + QuadraticEquation.getRoot1());
+        } else if (QuadraticEquation.getDelta() > 0) {
+            System.out.println("Nghiệm của PT là: " + QuadraticEquation.getRoot1() + " và " + QuadraticEquation.getRoot2());
         } else {
             System.out.println("PT vô nghiệm");
         }
--- 4.Class-objects/quadratic-equation/src/QuadraticEquation.java
+++ 4.Class-objects/quadratic-equation/src/QuadraticEquation.java
@@ -1,19 +1,30 @@
 public class QuadraticEquation {
-    private double a, b, c;
+    private static double a, b, c;
 
-    public QuadraticEquation(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    public double getDelta() {
-        return this.b * this.b - 4 * a * c;
-    }
-    public double getRoot1() {
-        return (-b + Math.pow(this.getDelta(), 0.5)) / (2 * a);
+    public QuadraticEquation() {
+
     }
 
-    public double getRoot2() {
-        return (-b - Math.pow(this.getDelta(), 0.5)) / (2 * a);
+    public static void setA(double a) {
+        QuadraticEquation.a = a;
+    }
+
+    public static void setB(double b) {
+        QuadraticEquation.b = b;
+    }
+
+    public static void setC(double c) {
+        QuadraticEquation.c = c;
+    }
+
+    public static double getDelta() {
+        return b * b - 4 * a * c;
+    }
+    public static double getRoot1() {
+        return (-b + Math.pow(getDelta(), 0.5)) / (2 * a);
+    }
+
+    public static double getRoot2() {
+        return (-b - Math.pow(getDelta(), 0.5)) / (2 * a);
     }
 }
