5aa07f01d0e87e1a172cbbd7411d838f0e84a293
test <nhuquangls@gmail.com>
2024-05-13 10:37:41 +0700

    13/05/24

---
+++ 5.Access-modifier/classes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.toString());
+        student.setClasses("C03");
+        student.setName("Quang");
+        System.out.println(student.toString());
+    }
+}
---
+++ 5.Access-modifier/classes/src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n" + this.classes;
+    }
+}
